466e1377d270a74edaa1947f0236077c2ae3de7c
ASUS <ASUS@LAPTOP-05EGR47L>
2026-02-04 18:09:45 +0530

    Sync latest Valentine changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import lovesvg from "./assets/All You Need Is Love SVG Cut File.svg";
 import lovesvg2 from "./assets/Love In The Air SVG Cut File.svg";
-import herPhoto from "./assets/chingi.jpeg";
-import romanticMusic from "./assets/munji.mp3";
+import herPhoto from "./assets/sushi.jpeg";
+import romanticMusic from "./assets/romantic.mp3";
 
 export default function App() {
     const [noCount, setNoCount] = useState(0);
@@ -67,7 +67,7 @@ export default function App() {
 
                     <img className="h-[230px] rounded-lg shadow-lg" src={herPhoto} />
                     <h1 className="text-4xl md:text-6xl my-4 text-center">
-                        Will you be my Valentine? chingi Darling
+                        Will you be my Valentine? seetha
                     </h1>
 
                     {showNiceTry && (
